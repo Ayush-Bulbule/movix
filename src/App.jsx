d70d0733f1965fd905898cf0d79b64487c433f5b
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-08-09 21:56:58 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      App
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/api.js
@@ -0,0 +1,24 @@
+import axios from axios
+
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+
+const headers = {
+    Authorization: `bearer ${TMDB_TOKEN}`,
+
+};
+
+export const fetchFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(`${API_BASE_URL}${url}`, {
+            headers,
+            params
+        })
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
