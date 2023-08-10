a8947620875a43f539a51276af283ad30a560791
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-08-11 02:02:52 +0530

    Added Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
+import { useEffect } from "react";
+import { fetchFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+//01:04:56
 function App() {
-  const [count, setCount] = useState(0)
+  const url = useSelector((state) => state.home.url);
+  console.log(url);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    apiTesting();
+  }, [])
+
+  const apiTesting = () => {
+    fetchFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    })
+  }
   return (
     <>
       App
+
     </>
   )
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-import axios from axios
+import axios from "axios"
 
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
